b678251bd341491539453a3dbc9a8856c34f0d06
Manuel <manuu.robles@gmail.com>
2026-04-25 00:07:55 -0300

    Add Vercel to CORS

--- src/main/java/com/techrepair/config/SecurityConfig.java
+++ src/main/java/com/techrepair/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://pcshop-rho.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
